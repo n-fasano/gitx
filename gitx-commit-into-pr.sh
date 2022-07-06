f5ec457759159675f56213a21f7c3e4283ffd383
Nicolas Fasano <nicolas.fasano@rebilly.com>
2022-07-06 16:07:17 +0300

    Initial commit

---
+++ gitx-commit-into-pr.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+help () {
+    echo "Usage:"
+    echo "gitx cpr <commit-message>"
+    echo
+    echo "Example:"
+    echo "gitx cpr \"Fix bug #531\""
+}
+
+MESSAGE="$1"
+[ -z "$MESSAGE" ] && help && return
+
+BRANCH="$(git branch --show-current)"
+
+git commit -m "$MESSAGE"
+git push
---
+++ gitx.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+help () {
+    echo \
+"   _____ _ _        
+  / ____(_) |       
+ | |  __ _| |___  __
+ | | |_ | | __\ \/ /
+ | |__| | | |_ >  < 
+  \_____|_|\__/_/\_\             
+
+
+Usage:
+gitx cp <commit-message>
+gitx cpr <commit-message>"
+}
+
+GITX_SCRIPTS="$(dirname "${BASH_SOURCE[0]}")"
+source "$GITX_SCRIPTS/config.sh"
+
+COMMAND="$1"
+shift
+
+case "$COMMAND" in
+    cp)
+        . "$GITX_SCRIPTS/gitx-commit-and-push.sh" $1
+    ;;
+
+    cpr)
+        . "$GITX_SCRIPTS/gitx-commit-into-pr.sh" $1
+    ;;
+
+    *)
+        help
+    ;;
+esac
